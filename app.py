32f922c9cfe90c43c66942ca128ff3ea13818801
willianchu <willianchu@hotmail.com>
2022-09-06 16:26:12 -0700

    add styles.css

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,8 +8,8 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/greet")
-def greet():
+@app.route("/register", methods=["POST"])
+def register():
   if not request.form.get("name") or request.form.get("sport") not in ["Kung-fu", "Karate", "Judo", "Capoeira"]:
     return render_template("failure.html")
 
