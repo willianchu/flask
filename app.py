fb53b741ced40595e06d4d91378c6fa658bf2f8a
willianchu <willianchu@hotmail.com>
2022-09-06 16:38:35 -0700

    set a global variable

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+  "Kung-fu",
+  "Karate",
+  "MuayTay",
+  "Capoeira",
+  "Jiujitsu"
+]
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
-  if not request.form.get("name") or request.form.get("sport") not in ["Kung-fu", "Karate", "Judo", "Capoeira"]:
+  if not request.form.get("name") or request.form.get("sport") not in SPORTS:
     return render_template("failure.html")
 
   return render_template("success.html")
