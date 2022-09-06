5b4cc53044fb4a746638bc6f1d2a027e92d097ee
willianchu <willianchu@hotmail.com>
2022-09-06 16:02:23 -0700

    create form

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  # name = request.args.get("name")
-  # , name=name)
-  return render_template("index.html")   
+  return render_template("index.html")
 
-@app.route("/greet", methods=["POST"])
-def greet():
-  name = request.args.get("name", "What ever!")
-  return render_template("greet.html", name=name)
+  
