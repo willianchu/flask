af56434853397d89646384386e9d8b59b16243ba
willianchu <willianchu@hotmail.com>
2022-09-06 16:09:23 -0700

    check values

--- app.py
+++ app.py
@@ -7,4 +7,9 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-  
+@app.route("/greet")
+def greet():
+  if not request.form.get("name") or request.form.get("sport") not in ["Kung-fu", "Karate", "Judo", "Capoeira"]:
+    return render_template("failure.html")
+
+  return render_template("success.html")
