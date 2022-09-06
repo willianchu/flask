0d3c6ffe0d85959c774b38354fd778aa4fe9d785
willianchu <willianchu@hotmail.com>
2022-09-06 15:37:40 -0700

    changed to POST

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
   # , name=name)
   return render_template("index.html")   
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
   name = request.args.get("name", "What ever!")
   return render_template("greet.html", name=name)
